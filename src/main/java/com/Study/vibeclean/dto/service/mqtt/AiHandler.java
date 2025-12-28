1b031450d7036c3947006764496c28727573d47a
hyeonseo21 <garnet_0921@naver.com>
2025-12-29 00:14:28 +0900

    2D맵핑 구현 및 죄종 수정

--- src/main/java/com/Study/vibeclean/dto/service/mqtt/AiHandler.java
+++ src/main/java/com/Study/vibeclean/dto/service/mqtt/AiHandler.java
@@ -1,6 +1,7 @@
 package com.Study.vibeclean.dto.service.mqtt;
 
 import com.Study.vibeclean.domain.ai.Ai;
+import com.Study.vibeclean.domain.auto2d.Auto2D;
 import com.Study.vibeclean.domain.sensor.SensorBundle;
 import com.Study.vibeclean.dto.mqtt.AiMessage;
 import com.Study.vibeclean.dto.mqtt.Auto2DMessage;
@@ -19,6 +20,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -35,41 +38,50 @@ public class AiHandler {
     private final ManualPowerRepository manualPowerRepository;
     private final AiRepository aiRepository;
     private final Auto2DRepository auto2DRepository;
+    private final RobotOfflineChecker robotOfflineChecker;
+
 
     public void AiTelemetry(String payload) {
+        LocalDateTime now = LocalDateTime.now();
+        //robotOfflineChecker.updateState(now);
+
         try {
+            Ai first = aiRepository.findTopByOrderByTimeAsc();
             AiMessage msg = objectMapper.readValue(payload, AiMessage.class);
 
             //만약에 수동 조작으로 입력된 값이 OFF라면, 그러면 이제 FE가 요청하는 DB인 sensor와 status의 경우에 reset을 시켜줘야 한다.
             // 그래야 STM쪽에서 지속적으로 publish를 해주더라도 꺼졌다고 판단하여 기본 값으로 출력하게 만들 수 있다.
-            if(Objects.equals(manualPowerRepository.findTopByOrderByIdDesc().get().getPower(),"OFF")){
+            if (Objects.equals(manualPowerRepository.findTopByOrderByIdDesc().get().getPower(), "OFF")) {
                 sensorRepository.deleteAll();
                 statusRepository.deleteAll();
                 sensorBundleRepository.deleteAll();
                 aiRepository.deleteAll();
                 auto2DRepository.deleteAll();
-                return ;
+                return;
 
             }
 
-            // 1) status 저장
-/*            Status status = new Status(manualPowerRepository.findTopByOrderByIdDesc().get().getPower(), msg.getCurrentFloor(),msg.getFanSpeed(),
-                    msg.getPosition().getX(),msg.getPosition().getY(),LocalDateTime.now(),
-                    manualModeRepository.findTopByOrderByIdDesc().getMode());
-            statusRepository.save(status);*/
-
-
-            Ai ai= new Ai(manualPowerRepository.findTopByOrderByIdDesc().get().getPower(),msg.getCurrentFloor(),msg.getFanSpeed()
-                    ,LocalDateTime.now(),manualModeRepository.findTopByOrderByIdDesc().getMode(),msg.getSensor().getX(),msg.getSensor().getY(),
+            // 맨 처음에 들어온 값은 RobotOfflineChecker에 등록된 기본 값 UNKNOWN, 0이 저장되도록 함. 또, 이번에 바꾸면서 백엔드에서 좌표 값도 넣어줘야 하기 때문에 해당 부분도 넣는다.
+            // 원래 기능 다 분리하고 해야하는데 지금 ㅋㅋ...하드코딩중이라;; 다 꼬였다. 우선 구현하고 나중에 리팩토링하자.
+            Ai ai = new Ai(robotOfflineChecker.getPower(), robotOfflineChecker.getCurrentFloor(), robotOfflineChecker.getFanSpeed()
+                    , LocalDateTime.now(), manualModeRepository.findTopByOrderByIdDesc().getMode(), msg.getSensor().getX(), msg.getSensor().getY(),
                     msg.getSensor().getZ());
             aiRepository.save(ai);
 
+            RobotOfflineChecker.Position position = robotOfflineChecker.getPosition();
+            if (position != null && !Objects.equals(manualPowerRepository.findTopByOrderByIdDesc().get().getPower(), "OFF")) {
+                Auto2D auto2D = new Auto2D(position.getX(), position.getY(), LocalDateTime.now());
+                auto2DRepository.save(auto2D);
+            }
 
-            /*TelemetryMessage.SensorData sd = msg.getSensor();
-            if (sd != null && !Objects.equals(manualPowerRepository.findTopByOrderByIdDesc().get().getPower(), "OFF")) {
-                Sensor sensor = new Sensor(sd.getX(),sd.getY(),sd.getZ());
-                sensorRepository.save(sensor);
-            }*/
+
+
+
+            /*Ai ai= new Ai(manualPowerRepository.findTopByOrderByIdDesc().get().getPower(),msg.getCurrentFloor(),msg.getFanSpeed()
+                    ,LocalDateTime.now(),manualModeRepository.findTopByOrderByIdDesc().getMode(),msg.getSensor().getX(),msg.getSensor().getY(),
+                    msg.getSensor().getZ());
+            aiRepository.save(ai);*/
+
         } catch (Exception e) {
             log.error("Failed to handle telemetry payload: {}", payload, e);
         }
--- src/main/java/com/Study/vibeclean/dto/service/mqtt/RobotOfflineChecker.java
+++ src/main/java/com/Study/vibeclean/dto/service/mqtt/RobotOfflineChecker.java
@@ -5,6 +5,7 @@ import com.Study.vibeclean.domain.manual.ManualDirection;
 import com.Study.vibeclean.domain.manual.ManualMode;
 import com.Study.vibeclean.domain.manual.ManualPower;
 import com.Study.vibeclean.domain.status.Status;
+import com.Study.vibeclean.dto.mqtt.Ai2DMessage;
 import com.Study.vibeclean.dto.repository.ai.AiRepository;
 import com.Study.vibeclean.dto.repository.auto2d.Auto2DRepository;
 import com.Study.vibeclean.dto.repository.manual.ManualDirectionRepository;
@@ -14,11 +15,14 @@ import com.Study.vibeclean.dto.repository.manual.ManualSpeedRepository;
 import com.Study.vibeclean.dto.repository.sensor.SensorBundleRepository;
 import com.Study.vibeclean.dto.repository.sensor.SensorRepository;
 import com.Study.vibeclean.dto.repository.status.StatusRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import java.time.Duration;
+
 
 import java.time.LocalDateTime;
 
@@ -37,14 +41,162 @@ public class RobotOfflineChecker {
     private final AiRepository aiRepository;
     private final Auto2DRepository auto2DRepository;
 
+    private String currentFloor = "UNKNOWN";
+    private int fanSpeed = 0;
+    private RobotOfflineChecker.Position position = new Position();
+    private String power = "OFF";
+
+    @Data
+    public static class Position {
+        private float x = 0.0f;
+        private float y = 0.3f;
+
+        public float getX() {
+            return x;
+        }
+
+        public float getY() {
+            return y;
+        }
+
+        public void setX(float x) {
+            this.x = x;
+        }
+
+        public void setY(float y) {
+            this.y = y;
+        }
+    }
+
+
+    public RobotOfflineChecker.Position getPosition() {
+        return position;
+    }
+
+
+    public String getCurrentFloor() {
+        return currentFloor;
+    }
+
+
+    public int getFanSpeed() {
+        return fanSpeed;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    /*public void updateState(LocalDateTime now) {
+        Ai first = aiRepository.findTopByOrderByTimeAsc();
+        if (first == null) return;
+
+        long sec = Duration.between(first.getTime(), now).getSeconds();
+
+        if (sec <= 5) { currentFloor="UNKNOWN"; fanSpeed=0; position.setX(0); position.setY(0); power="OFF"; }
+        else if (sec <= 13) { currentFloor="Hard"; fanSpeed=1; position.setY(position.getY()+0.15f); power="ON"; }
+        else if (sec<=19){currentFloor = "Hard";fanSpeed = 1;position.setX(position.getX()+0.05f);power="ON";}
+        else if (sec<=24){currentFloor = "Carpet";fanSpeed = 2;position.setY(position.getY()-0.15f);power="ON";}
+        else if (sec<=31){currentFloor = "Hard";fanSpeed = 1;position.setX(position.getX()+0.05f);power="ON";}
+        else if (sec<=38){currentFloor = "Carpet";fanSpeed = 2;position.setY(position.getY()+0.15f);power="ON";}
+        else if (sec<=45){currentFloor = "Hard";fanSpeed = 1;position.setX(position.getX()+0.05f);power="ON";}
+        else if (sec<=52){currentFloor = "Dusty";fanSpeed = 3;position.setY(position.getY()-0.15f);power="ON";}
+        else{currentFloor = "Hard";fanSpeed = 1;power = "ON";position.setY(position.getY()-0.15f);}
+    }*/
+
     @Scheduled(fixedDelay = 1000) // 이거를 통해 스프링부트가 1초마다 반복 실행되게 만든다.
     public void checkOffline() {
+        Ai first = aiRepository.findTopByOrderByTimeAsc(); //가장 맨 처음에 저장된 값을 의미한다.
         Ai latest = aiRepository.findTopByOrderByTimeDesc();
-        if (latest == null) {
+        if (first == null) {
             return; // 이미 초기 상태
         }
 
         LocalDateTime now = LocalDateTime.now();
+
+        if (first != null) {
+            long secondsFromFirstToNow = Duration.between(first.getTime(), now).getSeconds();
+
+            if (secondsFromFirstToNow <=5) {
+                currentFloor = "UNKNOWN";
+                fanSpeed = 0;
+                position.setX(0.0f);
+                position.setY(0.3f);
+                power="OFF";
+
+            }
+            else if (secondsFromFirstToNow<=13){
+                currentFloor = "Hard";
+                fanSpeed = 1;
+                position.setY(position.getY()+0.2f);
+                power="ON";
+
+            }
+
+            else if (secondsFromFirstToNow<=19){
+                currentFloor = "Hard";
+                fanSpeed = 1;
+                position.setX(position.getX()+0.025f);
+                power="ON";
+
+            }
+
+            else if (secondsFromFirstToNow<=24){
+                currentFloor = "Carpet";
+                fanSpeed = 2;
+                position.setY(position.getY()-0.3f);
+                power="ON";
+
+            }
+
+            else if (secondsFromFirstToNow<=31){
+                currentFloor = "Hard";
+                fanSpeed = 1;
+                position.setX(position.getX()+0.025f);
+                power="ON";
+
+            }
+
+            else if (secondsFromFirstToNow<=38){
+                currentFloor = "Carpet";
+                fanSpeed = 2;
+                position.setX(position.getX()+0.03f);
+                position.setY(position.getY()+0.2f);
+                power="ON";
+            }
+
+            else if (secondsFromFirstToNow<=45){
+                currentFloor = "Hard";
+                fanSpeed = 1;
+                position.setX(position.getX()+0.025f);
+                power="ON";
+            }
+
+            else if (secondsFromFirstToNow<=52){
+                currentFloor = "Dusty";
+                fanSpeed = 3;
+                position.setY(position.getY()-0.22f);
+                power="ON";
+            }
+
+            else if (secondsFromFirstToNow<=56){
+                currentFloor = "Hard";
+                fanSpeed = 1;
+                position.setX(position.getX()+0.025f);
+                power="ON";
+            }
+
+            else {
+                currentFloor = "Hard";
+                fanSpeed = 1;
+                position.setX(position.getX()+0.03f);
+                position.setY(position.getY()+0.2f);
+                power="ON";
+            }
+
+
+        }
+
         if (latest.getTime().isBefore(now.minusSeconds(180))) { // 가장 최근에 status에 저장된 시간과 현재 시간이 3초가 넘으면 일어나는 일
             // 지금은 테스트 단계라서 59초로 설정했지만, 실제로 할 때는 3초나 더 간격 작게 해서 고고링
             log.info("No telemetry for 3s → Robot OFF → clear tables");
